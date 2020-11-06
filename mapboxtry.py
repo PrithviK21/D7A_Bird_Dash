173074aa1e03ef9346c4e9386f3c0573eee41cf1
Prithvi Kumar <prithvikumar@hotmail.com>
2020-11-06 10:12:18 +0530

    Tried out some css and color things

--- mapboxtry.py
+++ mapboxtry.py
@@ -9,9 +9,9 @@ token = 'pk.eyJ1IjoicHJpdGh2aWsyMSIsImEiOiJja2g0eHBpamkwYXB5MnNrMDNjaXFvNnRhIn0.
 px.set_mapbox_access_token(token)
 global df
 df = pd.read_csv("finalMergedBirds/final_birds_fixed_dates.csv")
-#df = df.loc[df['Common_Name'] == 'Greater Flamingo']
-df['Date'] = df['Date'].apply(lambda x:  (int)(x.split('/')[2]))
-df = df.sort_values('Date',ascending=True)
+# df = df.loc[df['Common_Name'] == 'Greater Flamingo']
+df['Date'] = df['Date'].apply(lambda x: (int)(x.split('/')[2]))
+df = df.sort_values('Date', ascending=True)
 fig = px.scatter_mapbox(
     df,
     lat=df['Latitude'],
@@ -20,16 +20,24 @@ fig = px.scatter_mapbox(
     animation_frame="Date",
     animation_group='Common_Name',
     width=800,
-    height=600
-    )
-fig.update_layout(mapbox_style='dark')
-fig.update_layout(margin=dict(t=0,b=0,l=0,r=0))
+    height=600,
+)
+fig.update_layout(mapbox_style='dark', paper_bgcolor='#72aeb2')
+fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
 app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
-    dcc.Graph(id='mapboi', figure=fig),
-])
+    html.Div([dcc.Graph(id='mapboi', figure=fig)],
+             style={'top': '5%', 'left': '22%', 'margin': 'auto', 'position': 'absolute',
+                    'box-shadow': '5px 5px #363636'}),
+    html.Footer(
+        ['Copyright my foot'],
+        style={'text-align': 'center', 'position': 'absolute', 'bottom': '0',
+               'width': '100%', 'height': '2.5rem',
+               'background-color': 'DarkSalmon', 'color': 'white'}
+    )
+], style={'background-color': '#385d5f', 'position': 'relative','min-height': '100vh'})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
