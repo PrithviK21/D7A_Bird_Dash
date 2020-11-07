2f58cb8b618f24a45e8c8e1b09e4dfec0d0e1c60
Prithvi Kumar <prithvikumar@hotmail.com>
2020-11-07 22:26:59 +0530

    added welcome page, added logo text to map

--- mapboxtry.py
+++ mapboxtry.py
@@ -3,13 +3,11 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
-
-# setting access token
+#setting access token
 token = 'pk.eyJ1IjoicHJpdGh2aWsyMSIsImEiOiJja2g0eHBpamkwYXB5MnNrMDNjaXFvNnRhIn0.6eeLvU-4xuLb8q43RAQGBA'
 px.set_mapbox_access_token(token)
 global df
 df = pd.read_csv("finalMergedBirds/final_birds_fixed_dates.csv")
-# df = df.loc[df['Common_Name'] == 'Greater Flamingo']
 df['Date'] = df['Date'].apply(lambda x: int(x.split('/')[2]))
 df = df.sort_values('Date', ascending=True)
 fig = px.scatter_mapbox(
@@ -20,7 +18,7 @@ fig = px.scatter_mapbox(
     width=800,
     height=600,
 )
-fig.update_layout(mapbox_style='dark', paper_bgcolor='#72aeb2')
+fig.update_layout(mapbox_style='dark',paper_bgcolor='#96dcd4')
 fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
 app = dash.Dash(__name__)
 server = app.server
@@ -29,6 +27,7 @@ app.layout = html.Div(
     [
         html.Header([
             html.Div([
+                html.Div([html.A('Bird Name', href='#')], className='logo'),
                 html.Nav([
                     html.A('Map', href='#'),
                     html.A('Graphs', href='#'),
@@ -42,8 +41,7 @@ app.layout = html.Div(
             ['Copyright my foot'],
             className='footer',
         )
-
-    ], style={'background-color': '#385d5f', 'position': 'relative', 'min-height': '100vh'}
+    ], style={'background-color': '#449bb3', 'height': '100vh'}
 )
 
 if __name__ == '__main__':
