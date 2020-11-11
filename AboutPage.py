9371e818a0609ca7de921d9386f0be2ca1b6fc26
Prithvi Kumar <prithvikumar@hotmail.com>
2020-11-11 12:48:21 +0530

    Added about page content and styled it

--- AboutPage.py
+++ AboutPage.py
@@ -24,16 +24,41 @@ app.layout = html.Div(
         html.Div([
             html.Div([
                 html.H2("What is this app?"),
-                html.P("App big good read map see bird"),
+                html.P("This app is created with a purpose of reaching out to the bird enthusiast in all of us. This "
+                       "app will help you access all the data on bird sightings and population distribution in "
+                       "Maharashtra for the 7 species we have selected. We have created an interactive scatter map,"
+                       "a page to display graphs based on the data, filterable by date and species. Along with this, "
+                       "the original dataset is available on the DATASET page. "
+                       ),
+                html.Hr(),
+                html.H2("What did we use?"),
+                html.P(
+                    " Our primary framework was Dash. Dash is a free web application development framework that provides"
+                    " pure Python abstraction around HTML, CSS, and JavaScript. We had to learn HTML and CSS in order "
+                    "to use Dash. Additional resources include "
+                    "the use of Pycharm. To acquire datasets, we have used primarily three sites: ebird.org "
+                    "inaturalist.org twitter.com"),
+                html.Hr(),
                 html.H2("Who are we?"),
-                html.P("Big stressy bois kaam bohot diya humko.D7A SEM3")], className="content"),
-            ], className='wrapper'),
+                html.P(["We are second year students of Vivekananda Institute of Technology studying Computer Science."
+                        "This build is part of our mini-project assigned to us in our year and mentored by Mrs. "
+                        "Sharmila Sengupta. We are:",
+                        html.Br(),
+                        "J N Guru Akaash D7A 26 ",
+                        html.Br(),
+                        "Prithvi Kumar D7A 38 ",
+                        html.Br(),
+                        "Ashwin Kurup D7A 39 ",
+                        html.Br(),
+                        "Anurag Saraswat D7A 58 "]),
+            ], className="content2"),
+        ], className='wrapper'),
 
         html.Footer(
             ['Copyright my foot'],
             className='footer',
         )
-    ], style={'background-color': '#449bb3', 'height': '1100px'}
+    ], style={'background-color': '#449bb3', 'height': '1300px'}
 )
 
 if __name__ == '__main__':
