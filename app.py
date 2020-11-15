450028cc0194f5bed70084ee88be12fb9ecbd8f0
Prithvi Kumar <prithvikumar@hotmail.com>
2020-11-15 12:05:23 +0530

    Changed tab titles and all, added logo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import dash
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = dash.Dash(__name__, suppress_callback_exceptions=True, update_title="Loading...", title="Pakshirashtra")
 server = app.server
